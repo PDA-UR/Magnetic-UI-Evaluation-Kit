fd762e6dfbf51ff93abf2355aa6c721ecddf8769
Jürgen Hahn <eth3real89@gmail.com>
2020-12-17 12:00:49 +0100

    added csv file writing with demo parameters. added immediate flushing after appending text to file

--- server/main.py
+++ server/main.py
@@ -1,15 +1,20 @@
 from bottle import route, run, request, template
-
+import csv
+import threading
 
 if __name__ == '__main__':
+    log_file = open("log.csv", "a")
+    logger = csv.writer(log_file, delimiter=";")
+
     @route("/log/", method="POST")
     def log():
-        payload = request.json
-
-        print(payload)
+        log_data = request.json["data"]
+        logger.writerow([log_data["pid"], log_data["timestamp"], log_data["condition"]])
+        log_file.flush()
 
     @route("/")
     def index():
         return template("index.html", request=request)
 
-    run(host="localhost", port=3333, debug=False)
+    threading.Thread(target=run(host="localhost", port=3333, debug=False)).start()
+
